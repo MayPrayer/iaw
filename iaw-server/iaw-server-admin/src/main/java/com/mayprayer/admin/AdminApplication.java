82ae1d47fc664aa1cd2493a6101b0f3b76391277
shiwx <1652112896@qq.com>
2023-05-18 17:28:09 +0800

    项目初始化

---
+++ iaw-server/iaw-server-admin/src/main/java/com/mayprayer/admin/AdminApplication.java
@@ -0,0 +1,15 @@
+package com.mayprayer.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+//不加的话默认扫描com.mayprayer.admin下所有注解
+@ComponentScan(basePackages = "com.mayprayer.*")
+public class AdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminApplication.class,args);
+    }
+}
