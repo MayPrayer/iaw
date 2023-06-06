b03a39dee62ff63669e0afb1a03fe060af0582c8
shiwx <1652112896@qq.com>
2023-06-06 18:14:57 +0800

    研究了一下午，完了

---
+++ iaw-server/iaw-server-web/src/main/java/com/mayprayer/web/controller/RssController.java
@@ -0,0 +1,120 @@
+package com.mayprayer.web.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/rss")
+public class RssController {
+
+
+
+//    什么值得买
+//
+//    公众号
+//
+//    特殊网站
+//
+//    小黑盒    steam 自动领取
+//
+//    抢票软件 或者啥
+//
+//    米友社(元神，型穷铁道) 活动咨询
+//
+//    贴吧
+//
+//    小红书
+//
+//
+//    /**
+//     *
+//     */
+//    @PostMapping("/weibo/hot")
+//    public void weiboHot(){
+//
+//
+//
+//
+//    }
+//
+//
+//    /**
+//     * 微博用户id
+//     * @param userId
+//     */
+//    @PostMapping("/weibo/user/{userId}")
+//    public void weiboUser(@PathVariable String userId){
+//
+//
+//
+//
+//    }
+//
+//
+//    /**
+//     * 知乎热搜
+//     */
+//    @PostMapping("/zhihu/hot")
+//    public void zhihuHot(){
+//
+//
+//
+//
+//    }
+//
+//    /**
+//     * 知乎用户动态
+//     */
+//    @PostMapping("/zhihu/{userId}")
+//    public void zhihuHot(@PathVariable String userId){
+//
+//
+//
+//
+//    }
+//
+//    /**
+//     * b站用户状态
+//     */
+//    @PostMapping("/bilibili/{userId}")
+//    public void bilibiliUser(@PathVariable String userId){
+//
+//
+//
+//
+//    }
+//
+//    /**
+//     * b站用户状态
+//     */
+//    @PostMapping("/bilibili/hot")
+//    public void bilibiliHot(){
+//
+//
+//
+//
+//    }
+//
+//
+//    /**
+//     * b站用户番剧
+//     */
+//    @PostMapping("/bilibili/video/{userId}")
+//    public void bilibiliVideo(@PathVariable String userId){
+//
+//
+//
+//
+//    }
+//
+//
+//
+//
+//
+//
+//
+
+
+
+}
