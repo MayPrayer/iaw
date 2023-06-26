153d2d82b8b9610dc87386d61f5aad703eeeda18
shiwangxing <shiwangxing@56lcy.com>
2023-06-26 23:21:05 +0800

    一些工具测试案例

---
+++ iaw-server/iaw-server-admin/src/main/java/com/mayprayer/admin/service/M3U8Service.java
@@ -0,0 +1,13 @@
+package com.mayprayer.admin.service;
+
+/**
+ * @Author: shiwx
+ * @Description: TODO
+ * @Date: Created in 23:00 2023/6/6
+ * @Modified By:
+ */
+public interface M3U8Service {
+
+
+
+}
