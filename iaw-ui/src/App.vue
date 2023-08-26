363216369e25597b3178de046182cf071ebe99b7
shiwx <1652112896@qq.com>
2023-08-26 16:54:48 +0800

    配置vue-router

--- iaw-ui/src/App.vue
+++ iaw-ui/src/App.vue
@@ -1,14 +1,6 @@
-<script setup lang="ts">
-import {HomeOutlined} from   '@ant-design/icons-vue'
-</script>
-
 <template>
-  <div>
-    <a-button type="danger">你好呀 <home-outlined /></a-button>
 
+<router-view/>
 
-  </div>
 </template>
 
-<style scoped>
-</style>
--- iaw-ui/src/main.ts
+++ iaw-ui/src/main.ts
@@ -3,10 +3,14 @@ import App from './App.vue'
 import { createPinia } from "pinia";
 import Antd from "ant-design-vue";
 import 'ant-design-vue/dist/reset.css';
-
+import GolbalComponents from './components/index'
+import defaultcss from './styles/defalt.css'  //默认样式
+import router  from "./router/index";
 
 
 createApp(App)
     .use(createPinia())
     .use(Antd)
+    .use(router)
+    .use(GolbalComponents)
     .mount('#app')
