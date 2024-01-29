5ee3acd543ac3f85e21261bfc33d7b4ad0942a53
shiwx <1652112896@qq.com>
2024-01-29 11:39:59 +0800

    使用sm3签名摘要

--- iaw-server/iaw-server-system/src/main/java/com/mayprayer/system/config/SM3PasswordEncoder.java
+++ iaw-server/iaw-server-system/src/main/java/com/mayprayer/system/config/SM3PasswordEncoder.java
@@ -1,24 +1,21 @@
 package com.mayprayer.system.config;
 
 import cn.hutool.crypto.digest.DigestUtil;
+import cn.hutool.crypto.digest.SM3;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
-
-/**
- * 自定义加密方式
- */
-
 @Component
-public class Md5PasswordEncoder implements PasswordEncoder {
+public class SM3PasswordEncoder implements PasswordEncoder {
 
+    private SM3 sm3 = new SM3();
 
     @Override
     public String encode(CharSequence rawPassword) {
         if (rawPassword == null) {
             throw new IllegalArgumentException("输入密码不能为空");
         } else {
-            return DigestUtil.md5Hex((rawPassword.toString()));
+            return sm3.digestHex(rawPassword.toString());
         }
     }
 
