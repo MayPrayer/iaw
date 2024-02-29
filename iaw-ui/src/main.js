8c8a7e765d19752296fdda792a43bc72930eccd3
shiwx <1652112896@qq.com>
2024-02-29 10:55:32 +0800

    移除ui框架

--- iaw-ui/src/main.js
+++ iaw-ui/src/main.js
@@ -1,12 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import { createPinia } from "pinia";
-import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
 import vueRouter from  './router/index'
 
 createApp(App)
     .use(createPinia())
-    .use(ElementPlus)
     .use(vueRouter)
     .mount('#app')
